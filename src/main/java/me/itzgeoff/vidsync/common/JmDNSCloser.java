11441b61ead4100d70ed4945b9d0400c33e54a79
Geoff Bourne <itzgeoff@gmail.com>
2013-04-20 11:55:08 -0500

    Attempting to recover Linux usage of JmDNS after resume from suspend state

--- src/main/java/me/itzgeoff/vidsync/common/JmDNSCloser.java
+++ src/main/java/me/itzgeoff/vidsync/common/JmDNSCloser.java
@@ -1,29 +1,78 @@
+/**
+ * Copyright 2013 Geoff Bourne
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package me.itzgeoff.vidsync.common;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import javax.annotation.PreDestroy;
 import javax.jmdns.JmDNS;
+import javax.jmdns.JmDNS.Delegate;
+import javax.jmdns.ServiceInfo;
 
-import org.springframework.context.annotation.Scope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
-public class JmDNSCloser {
+public class JmDNSCloser implements Delegate {
+    
+    private static final Logger logger = LoggerFactory.getLogger(JmDNSCloser.class);
 
-	private JmDNS jmDNS;
+    private List<JmDNS> instances = 
+            Collections.synchronizedList(new ArrayList<JmDNS>());
 	
-	public JmDNS getJmDNS() {
-		return jmDNS;
-	}
-
-	public void setJmDNS(JmDNS jmDNS) {
-		this.jmDNS = jmDNS;
+	public void addJmDNS(JmDNS jmDNS) {
+		instances.add(jmDNS);
+		jmDNS.setDelegate(this);
 	}
 
 	@PreDestroy
 	public void close() {
-		if (jmDNS != null) {
-			jmDNS.unregisterAllServices();
-		}
+		synchronized (instances) {
+		    for (JmDNS jmDNS : instances) {
+                jmDNS.unregisterAllServices();
+            }
+        }
 	}
+
+    /* (non-Javadoc)
+     * @see javax.jmdns.JmDNS.Delegate#cannotRecoverFromIOError(javax.jmdns.JmDNS, java.util.Collection)
+     */
+    @Override
+    public void cannotRecoverFromIOError(JmDNS deadJmDNS, Collection<ServiceInfo> infos) {
+        try {
+            deadJmDNS.unregisterAllServices();
+            deadJmDNS.close();
+            instances.remove(deadJmDNS);
+        } catch (Exception e) {
+            logger.warn("Trying to clean up dead JmDNS", e);
+        }
+        
+        try {
+            JmDNS jmDNS = JmDNS.create();
+            for (ServiceInfo serviceInfo : infos) {
+                jmDNS.registerService(serviceInfo);
+            }
+            addJmDNS(jmDNS);
+        } catch (IOException e) {
+            logger.error("Failed to create new JmDNS", e);
+        }
+    }
 }
